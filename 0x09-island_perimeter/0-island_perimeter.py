779a649ab05e4964c1c64906e78c7e7d28c070a4
Karl <tekamkarl@gmail.com>
2024-06-28 02:21:57 +0100

    returns the perimeter of the island described in grid

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,11 +4,13 @@
 
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
-
-    if not grid or not grid[0]:
-        return 0
-
-    height = len(grid) - 2
-    width = len(grid[0]) - 2
-
-    return height * width
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
