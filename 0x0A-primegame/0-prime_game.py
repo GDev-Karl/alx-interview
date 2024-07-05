92c8f63af3aac27dc81f1bf38d3aab4ad2e69efd
Karl <tekamkarl@gmail.com>
2024-07-05 01:54:20 +0100

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,38 +1,45 @@
-def isWinner(x, nums):
-    if not nums or x < 1:
+#!/usr/bin/python3
+"""
+Define is_winner function, a solution to the Prime Game problem
+"""
+
+def generate_primes(n):
+    """Return list of prime numbers between 2 and n inclusive
+       Args:
+        n (int): upper boundary of range
+    """
+    primes_list = []
+    sieve = [True] * (n + 1)
+    for prime in range(2, n + 1):
+        if sieve[prime]:
+            primes_list.append(prime)
+            for i in range(prime * 2, n + 1, prime):
+                sieve[i] = False
+    return primes_list
+
+
+def is_winner(rounds, upper_limit):
+    """
+    Determines winner of Prime Game
+    Args:
+        rounds (int): no. of rounds of game
+        upper_limit (int): upper limit of range for each round
+    Return:
+        Name of winner (Maria or Ben) or None if winner cannot be found
+    """
+    if rounds is None or upper_limit is None or rounds == 0 or upper_limit == 0:
         return None
-    
-    max_n = max(nums)
-    
-    # Sieve of Eratosthenes to find all prime numbers up to max_n
-    is_prime = [True] * (max_n + 1)
-    is_prime[0] = is_prime[1] = False
-    for i in range(2, int(max_n**0.5) + 1):
-        if is_prime[i]:
-            for multiple in range(i * i, max_n + 1, i):
-                is_prime[multiple] = False
-
-    # Precompute the number of prime picks possible for each n
-    prime_counts = [0] * (max_n + 1)
-    for i in range(1, max_n + 1):
-        prime_counts[i] = prime_counts[i - 1] + (1 if is_prime[i] else 0)
-
-    maria_wins = 0
-    ben_wins = 0
-    
-    # Simulate each game
-    for n in nums:
-        if prime_counts[n] % 2 == 0:
-            ben_wins += 1
+    maria_score = 0
+    ben_score = 0
+    for _ in range(rounds):
+        primes = generate_primes(upper_limit)
+        if len(primes) % 2 == 0:
+            ben_score += 1
         else:
-            maria_wins += 1
-    
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+            maria_score += 1
+    if maria_score > ben_score:
+        return 'Maria'
+    elif ben_score > maria_score:
+        return 'Ben'
+    return None
 
-# Example usage
-print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))  # Should output "Winner: Ben"
