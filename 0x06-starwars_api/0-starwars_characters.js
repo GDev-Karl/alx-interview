ddb8a1cb097808148c0f6d860ba0e7a59bbbb0d5
Karl <tekamkarl@gmail.com>
2024-11-17 22:12:19 +0100

    Starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,49 +1,52 @@
 #!/usr/bin/node
+const request = require("request");
 
-const request = require('request');
 const movieId = process.argv[2];
-
 if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  console.error("Usage: ./0-starwars_characters.js <Movie ID>");
   process.exit(1);
 }
 
 const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-request(apiUrl, (err, res, body) => {
-  if (err) {
-    console.error(err);
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
     return;
   }
 
-  if (res.statusCode === 200) {
-    const movieData = JSON.parse(body);
-    const characters = movieData.characters;
-
-    // Fetch and print character names in order
-    fetchCharactersInOrder(characters, 0);
+  if (response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    if (characters.length > 0) {
+      printCharacters(characters, 0);
+    } else {
+      console.log("No characters found for this movie.");
+    }
   } else {
-    console.error(`Failed to retrieve movie data. Status code: ${res.statusCode}`);
+    console.error(
+      `Failed to fetch movie data. Status code: ${response.statusCode}`
+    );
   }
 });
 
-function fetchCharactersInOrder(characters, index) {
+function printCharacters(characters, index) {
   if (index >= characters.length) {
     return;
   }
 
-  request(characters[index], (err, res, body) => {
-    if (err) {
-      console.error(err);
+  request(characters[index], (error, response, body) => {
+    if (error) {
+      console.error(error);
       return;
     }
 
-    if (res.statusCode === 200) {
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-      fetchCharactersInOrder(characters, index + 1);
+    if (response.statusCode === 200) {
+      console.log(JSON.parse(body).name);
+      printCharacters(characters, index + 1);
     } else {
-      console.error(`Failed to retrieve character data. Status code: ${res.statusCode}`);
+      console.error(
+        `Failed to fetch character data. Status code: ${response.statusCode}`
+      );
     }
   });
 }
